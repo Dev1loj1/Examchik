4babdaca1bca141e62a851fa58d4ba7d0ba934b9
Andrey <andrenpolin1504@gmail.com>
2026-09-10 00:35:42 +0300

    добавлена реализация класса Треугольник

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
+
+class Triangle {
+private:
+    double side;
+    double height;
+
+public:
+    Triangle(double side, double height) {
+        setSide(side);
+        setHeight(height);
+    }
+
+    void setSide(double side) {
+        if (side <= 0) {
+            throw invalid_argument("Сторона должна быть больше нуля");
+        }
+
+        this->side = side;
+    }
+
+    void setHeight(double height) {
+        if (height <= 0) {
+            throw invalid_argument("Высота должна быть больше нуля");
+        }
+
+        this->height = height;
+    }
+
+    double getSide() const {
+        return side;
+    }
+
+    double getHeight() const {
+        return height;
+    }
+
+    double getArea() const {
+        return side * height / 2.0;
+    }
+};
 
 int main() {
     double side;
@@ -12,14 +53,15 @@ int main() {
     cout << "Введите высоту треугольника: ";
     cin >> height;
 
-    if (side <= 0 || height <= 0) {
-        cout << "Ошибка: сторона и высота должны быть больше 0." << endl;
-        return 1;
+    try {
+        Triangle triangle(side, height);
+
+        cout << "Площадь треугольника: "
+             << triangle.getArea() << endl;
     }
-
-    double area = side * height / 2.0;
-
-    cout << "Площадь треугольника: " << area << endl;
+    catch (const invalid_argument& error) {
+        cout << "Ошибка: " << error.what() << endl;
+    }
 
     return 0;
 }

bec8f9b945f2548a8467bf5d0435945e383ae26c
Andrey <andrenpolin1504@gmail.com>
2026-09-10 00:30:40 +0300

    Исправлен интерфейс пользователя

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+
 using namespace std;
 
+int main() {
+    double side;
+    double height;
 
-int main (){
-    cout << "hello boghghys" << endl;
+    cout << "Введите сторону треугольника: ";
+    cin >> side;
+
+    cout << "Введите высоту треугольника: ";
+    cin >> height;
+
+    if (side <= 0 || height <= 0) {
+        cout << "Ошибка: сторона и высота должны быть больше 0." << endl;
+        return 1;
+    }
+
+    double area = side * height / 2.0;
+
+    cout << "Площадь треугольника: " << area << endl;
+
     return 0;
 }
